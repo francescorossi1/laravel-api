ae1804cefe37a43fb44e3f8a27a2258b32197ec1
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-10-04 14:24:33 +0200

    first commit

---
+++ resources/js/front.js
@@ -0,0 +1,14 @@
+window.Vue = require('vue');
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+import App from './components/App.vue';
+
+const root = new Vue({
+    el: '#root',
+    render: h => h(App)
+});
---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="root"></div>
+@endsection
+
+@section('script-js')
+    <script src="{{ asset('js/front.js') }}" defer></script>
+@endsection
