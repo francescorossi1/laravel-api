30e9f012473295212945d59c7f2b3926eaf241a8
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-10-04 17:58:49 +0200

    created PostCard component, moved data from PostsList to PostCard

--- resources/js/components/posts/PostsList.vue
+++ resources/js/components/posts/PostsList.vue
@@ -1,19 +1,33 @@
 <template>
     <section>
         <div class="container mt-4">
-            <div class="card">
-                <div class="card-body">CardTest</div>
+            <div v-for="post in posts" :key="post.id">
+                <PostCard :post="post" />
             </div>
         </div>
     </section>
 </template>
 
 <script>
+import PostCard from './PostCard'
 export default {
     name: 'PostsList',
+    components: { PostCard },
+    data(){
+        return {
+            posts: []
+        }
+    },
+    methods: {
+        fetchPosts(){
+            axios.get('http://localhost:8000/api/posts')
+            .then(res => {
+                this.posts = res.data
+            })
+        }
+    },
+    mounted(){
+        this.fetchPosts();
+    }
 }
 </script>
-
-<style>
-
-</style>
