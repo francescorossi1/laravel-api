070da4949a9841b1e326d89c95d3bd0fe416a308
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-10-04 16:45:18 +0200

    added posts to controller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        return 'Test';
+        $posts = Post::all();
+        return response()->json($posts);
     }
 
     /**
