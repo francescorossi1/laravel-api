1eb374d9ea00612738117a3348125a017c101edd
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-10-05 18:05:43 +0200

    added Loader and catch to PostsList, PostDetailPage

--- resources/js/components/pages/PostDetailPage.vue
+++ resources/js/components/pages/PostDetailPage.vue
@@ -1,36 +1,51 @@
 <template>
     <section>
-        <PostCard :post="post" />
-        <div class="justify-content-end d-flex">
-            <router-link class="btn btn-info" :to="{ name: 'home' }">
-                Torna alla Home
-            </router-link>
+        <div v-if="isLoading">
+            <AppLoader />
+        </div>
+        <div v-else-if="!isLoading && post">
+            <PostCard :post="post" />
+            <div class="justify-content-end d-flex">
+                <router-link class="btn btn-info" :to="{ name: 'home' }">
+                    Torna alla Home
+                </router-link>
+            </div>
         </div>
     </section>
 </template>
 
 <script>
 import PostCard from '../posts/PostCard'
+import AppLoader from '../AppLoader.vue'
 export default {
     name: 'PostDetailPage',
-    components: { PostCard },
+    components: { PostCard, AppLoader },
     data() {
         return {
-            post: null
+            post: null,
+            isLoading: false
         }
     },
     methods: {
         fetchPost() {
+            this.isLoading = true;
             axios.get('http://localhost:8000/api/posts/' + this.$route.params.id)
-                .then(res => {
+            .then(res => {
                     this.post = res.data
                 })
-        }
-    },
+                .catch((err) => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.isLoading = false
+                })
+    }
+},
     mounted() {
         this.fetchPost();
     }
 }
+
 </script>
 
 <style>
--- resources/js/components/posts/PostsList.vue
+++ resources/js/components/posts/PostsList.vue
@@ -1,6 +1,9 @@
 <template>
     <section>
-        <div class="container mt-4">
+        <div v-if="isLoading">
+            <AppLoader />
+        </div>
+        <div v-else-if="!isLoading && posts" class="container mt-4">
             <div v-for="post in posts" :key="post.id">
                 <PostCard :post="post" />
                 <div class="d-flex justify-content-end mb-5">
@@ -14,19 +17,28 @@
 
 <script>
 import PostCard from './PostCard'
+import AppLoader from '../AppLoader'
 export default {
     name: 'PostsList',
-    components: { PostCard },
+    components: { PostCard, AppLoader },
     data() {
         return {
-            posts: []
+            posts: [],
+            isLoading: false
         }
     },
     methods: {
         fetchPosts() {
+            this.isLoading = true;
             axios.get('http://localhost:8000/api/posts')
                 .then(res => {
                     this.posts = res.data
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.isLoading = false
                 })
         }
     },
